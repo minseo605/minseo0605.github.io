1813b11a219ebc30fa12e9fcc2e89cf56cedf983
minseo605 <124219866+minseo605@users.noreply.github.com>
2023-03-16 16:56:43 +0900

    Add files via upload

---
+++ vue/instaData.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: "hani",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: "https://placeimg.com/640/480/animals",
+        likes: 200,
+        liked: false,
+        date: new Date().toLocaleDateString(),
+        content: "lalalalala",
+        filter: "perpetua"
+    },
+    {
+        name: "mingee",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: "https://placeimg.com/640/480/animals",
+        likes: 300,
+        liked: false,
+        date: new Date().toLocaleDateString(),
+        content: "nananan",
+        filter: "perpetua"
+    },
+    {
+        name: "hearin",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: "https://placeimg.com/640/480/animals",
+        likes: 400,
+        liked: false,
+        date: new Date().toLocaleDateString(),
+        content: "iiooioioooi",
+        filter: "perpetua"
+    }
+]
